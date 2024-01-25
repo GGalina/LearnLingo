423c16ab825bd40cb68bf161c8fc9dcf6c5a286e
GGalina <71042425+GGalina@users.noreply.github.com>
2024-01-25 07:21:53 +0400

    ADD: Changes for deployment

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
+//import { useMediaQuery } from 'react-responsive';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/Header/Header';
 // import { Container } from 'components/Container';
 
 export const HomePage = () => {
-  const isDesktopSize = useMediaQuery({ query: '(min-width: 1280px)' });
+ // const isDesktopSize = useMediaQuery({ query: '(min-width: 1280px)' });
 
   return (
     <div>
